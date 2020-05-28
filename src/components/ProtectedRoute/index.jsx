a3b3dde2cf55d6434fed4ff8f535637f64a29b46
albert85 <tmtpalbert@gmail.com>
2020-05-28 18:21:54 +0100

    bg(protected-route): add protection to route
    - add restriction to unauthorised user
    - redirect signup to login for user to login properly
    
    [Finishes #27]

---
+++ src/components/ProtectedRoute/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import Auth from "../Auth";
+import AppLayout from "../Layout/AppLayout";
+
+
+function ProtectedRoute({ component: Component, ...rest }) {
+  return (
+    <Route {...rest}
+      render={(props) => {
+        if (Auth.isAuthenticated()) {
+          return (
+            <AppLayout headerTitle={rest.page} {...props}>
+              <Component {...props} />
+            </AppLayout>
+          );
+        } else {
+          return (<Redirect to={
+            {
+              pathname: "/login",
+              state: {
+                from: props.location
+              }
+            }
+          } />
+          );
+        }
+      }}
+    />
+  );
+}
+
+export default ProtectedRoute;
